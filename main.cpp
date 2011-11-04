a01e595c020bc591034ce28898bf94e9ee59392b
Jan Vlnas <git@jan.vlnas.cz>
2011-11-04 19:13:45 +0100

    Init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: j
+ *
+ * Created on 4. listopad 2011, 19:09
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
